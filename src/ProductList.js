ad6058324a5e8a83943c81ccb721912d063d8269
Vaibhav-Incronix <vaibhav.incronix@gmail.com>
2024-02-21 12:56:43 +0530

    donee

---
+++ src/ProductList.js
@@ -0,0 +1,24 @@
+//ProductList.js
+import React from 'react';
+import './assets/product.css';
+
+const ProductList = ({ products }) => {
+
+
+    return (
+        <div className='product-list'>
+            <ul className="card-info-container">
+
+            {products.map(product => (
+                    <li key={product.id} className='product-item'>
+                        <img src={product.image} alt={product.name} />
+                        <div className="overlay">{product.model}</div>
+                        <p>{product.name}</p>
+                    </li>
+                ))}
+                </ul>
+        </div>
+    )
+}
+
+export default ProductList;
---
+++ src/SunglassCategoryList.js
@@ -0,0 +1,30 @@
+//SunglassCategoryList.js
+import React from "react";
+import './assets/product.css';
+
+const SunglassCategoryList = ({ categories, onSelect }) => {
+
+
+    return (
+        <div className="category-card">
+            <h2>Category</h2>
+            <ul>
+            <li key="all">
+                    <button className='category-button' onClick={() => onSelect(null)}>
+                        <p>All</p>
+                    </button>
+                </li>
+                
+                {categories.map(category => (
+                    <li key={category.id}>
+                        <button className='category-button' onClick={() => onSelect(category)}>
+                            <p>{category.name}</p>
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default SunglassCategoryList;
