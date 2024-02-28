e3c684197a5e7249b027abf3a8b9f3f234dd780e
Supriya Shinde <supriya.incronix@gmail.com>
2024-02-28 16:20:28 +0530

    change in product.css

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -6,7 +6,7 @@ const CategoryList = ({ categories, selectedCategory, onSelect }) => {
   return (
     <div className="category-list-container">
       <ul className="category-list">
-        <p className="category-header">Categories</p>
+        <p className="category-header">Categories :</p>
         <li
           className={selectedCategory === "" ? "active" : ""}
           onClick={() => onSelect("")}
