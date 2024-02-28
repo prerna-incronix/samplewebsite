d7c082b4e19596716317322b3ea2eab0c2fb1add
Supriya Shinde <supriya.incronix@gmail.com>
2024-02-28 10:59:59 +0530

    product.css changes

--- src/SunglassCategoryList.js
+++ src/SunglassCategoryList.js
@@ -6,25 +6,17 @@ import './assets/product.css'; // Import the CSS file
 const SunglassCategoryList = ({ categories, onSelect }) => {
     return (
         <div className="category-list-container"> {/* Apply category-list-container class */}
-        
-            <ul className="category-list"> {/* Apply category-list class */}
-                <li onClick={() => onSelect("")}>All</li>
-        <div className="category-list-container"> {/* Apply category-list-container class */}
-        
-            <ul className="category-list"> {/* Apply category-list class */}
+           <ul className="category-list"> {/* Apply category-list class */}
             <p className="category-list">Categories:</p>
                 <li onClick={() => onSelect("")}>All</li>
-                {categories.map(category => (
-                    <li key={category.id} onClick={() => onSelect(category.name)}>
-                        {category.name}
-                    <li key={category.id} onClick={() => onSelect(category.name)}>
-                        {category.name}
-                    </li>
-                ))}
-            </ul> 
+                    {categories.map(category => (
+                            <li key={category.id} onClick={() => onSelect(category.name)}>
+                                {category.name}
+                            </li>
+                    ))}
             </ul> 
         </div>
     );
 }
 
-export default SunglassCategoryList;
+                export default SunglassCategoryList;
