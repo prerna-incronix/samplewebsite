c14baa887d226192fd226099d61cc6cb1635eea6
Shubham <shubham.incronix@gmail.com>
2024-02-28 10:54:59 +0530

    ggg

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -2,20 +2,12 @@ import React from "react";
 import './assets/product.css';
 
 const CategoryList = ({ categories, selectedCategory, onSelect }) => {
-const CategoryList = ({ categories, selectedCategory, onSelect }) => {
     return (
         <div className="category-list-container">
-           
             <ul className="category-list">
             <p className="category-header">Categories</p>
                 <li className={selectedCategory === "" ? "active" : ""} onClick={() => onSelect("")}>All</li>
                 {categories.map(category => (
-                    <li
-                        key={category.id}
-                        className={selectedCategory === category.name ? "active" : ""}
-                        onClick={() => onSelect(category.name)}
-                    >
-                        {category.name}
                     <li
                         key={category.id}
                         className={selectedCategory === category.name ? "active" : ""}
